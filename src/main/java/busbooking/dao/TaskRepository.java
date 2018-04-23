9c6db978b2ad47a4d62a5dfa2b3b9fad70c9637b
Pawan Gaur <pawangaur001@gmail.com>
2018-04-23 18:12:22 +0530

    Delete TaskRepository.java

--- src/main/java/busbooking/dao/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package busbooking.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import busbooking.model.Task;
-
-public interface TaskRepository extends CrudRepository<Task, Integer> {
-	
-}
